dde2713155d62e7ffe3f38002952af8a72f6106d
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-02-06 14:03:59 +0100

    Added instance of KlasseRum and new students

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassRoomNet60
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            KlasseRum klasseRum = new KlasseRum();
+            klasseRum.KlasseNavn = "3B";
+            klasseRum.SemesterStart = new System.DateTime(2022,9,1);
+            klasseRum.KlasseListe.Add(new Studerende("Rebekka", 2, 14));
+            klasseRum.KlasseListe.Add(new Studerende("Cecilia", 3, 5));
+            klasseRum.KlasseListe.Add(new Studerende("Sara", 4, 20));
+        }
+    }
+}
